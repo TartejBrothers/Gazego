2719d1c38268e74470ef121a509f8e07fa210520
Taranjeet Singh Bedi <taranjeetsinghbedi2@gmail.com>
2024-11-09 02:36:37 +0530

    Updated: UI

--- src/components/elements/gpaypopup.jsx
+++ src/components/elements/gpaypopup.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-
-export default function GpayPopup() {
+import { useNavigate } from "react-router-dom";
+export default function GpayPopup({ isVisible, onClose, amount, phoneNumber }) {
+  const navigate = useNavigate();
   return (
-    <div className="popup-overlay">
-      <div className="gpaypopup">
-        
+    <div className="popup-overlay" onClick={onClose}>
+      <div className="gpaypopup" onClick={(e) => e.stopPropagation()}>
+        <div className="gpaypopup-header">
+          <h2>Payment Confirmation</h2>
+          <div className="amounttotalpopup">
+            <p>Amount:</p>
+            <p>{amount}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
